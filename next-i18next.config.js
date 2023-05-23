ec463797e080d15b528bb7cc7eecce4360bb0ad0
heru-nan <hrwgallardo@gmail.com>
2023-05-23 16:13:05 -0400

    change default language

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en-US',
-    locales: ['en-US', 'de-DE'],
+    defaultLocale: 'es-CL',
+    locales: ['es-CL','en-US', 'de-DE'],
     localeDetection: false,
     localePath: path.resolve('./public/locales'),
   },
