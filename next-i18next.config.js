3d332d65f3dffd9b96b3314e6036e7dc08fda628
heru-nan <hrwgallardo@gmail.com>
2023-05-24 20:46:25 -0400

    remove de

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'es-CL',
-    locales: ['es-CL','en-US', 'de-DE'],
+    locales: ['es-CL','en-US'],
     localeDetection: false,
     localePath: path.resolve('./public/locales'),
   },
